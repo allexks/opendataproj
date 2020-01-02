7d91ea4d6ae6e8eb9e5619d8258704b4b977ae74
Alexander Ignatov <ani1802@abv.bg>
2020-01-02 17:01:12 +0200

    Initial Commit

---
+++ Open Data Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open Data Project
+//
+//  Created by Yalishanda on 2.01.20.
+//  Copyright © 2020 fmi-swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
